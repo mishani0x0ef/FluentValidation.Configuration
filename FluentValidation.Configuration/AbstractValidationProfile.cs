65fb2a0c96f5d36640cc01db4c5d80ba48ff9590
mykhailo.romaniuk <mishani0x0ef@gmail.com>
2016-09-11 00:07:20 +0300

    Implement validation profile infrastructure.

---
+++ FluentValidation.Configuration/AbstractValidationProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FluentValidation.Configuration
+{
+    public abstract class AbstractValidationProfile : IValidationProfile
+    {
+        public IValidationConfiguration Configuration { get; protected set; }
+
+        protected AbstractValidationProfile()
+        {
+            Configuration = new ValidationConfiguration();
+        }
+
+        protected AbstractValidationProfile(IValidationConfiguration config)
+        {
+            if(config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            Configuration = config;
+        }
+    }
+}
---
+++ FluentValidation.Configuration/IValidationProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace FluentValidation.Configuration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IValidationProfile
+    {
+        /// <summary>
+        /// Gets instance of configuration which contains validators.
+        /// </summary>
+        IValidationConfiguration Configuration { get; }
+    }
+}
